00dbc9b2229a288718b42301bd43acf142d7fec2
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2024-03-11 21:17:47 -0500

    Arreglo para despliegue

--- api/app.py
+++ api/app.py
@@ -174,3 +174,6 @@ def chat():
 def home():
     return "Back del proyecto de inteligencia"
 
+
+if __name__ == '__main__':
+    app.run()
--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 # run.py
+import os
 
-from api.wsgi import app
+from api.app import app
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT")))
